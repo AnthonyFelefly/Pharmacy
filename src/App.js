74776c78924c16dca6693b55f84751c120225d64
AnthonyFelefly <anthony.felefly@net.usj.edu.lb>
2021-10-16 01:50:30 +0300

    new name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Pharmacy Premier soin</NavbarBrand>
+            <NavbarBrand href="/">Pharmacy first care</NavbarBrand>
           </div>
         </Navbar>
       </div>
